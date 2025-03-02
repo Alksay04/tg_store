9f22e41873d76b3178251684c2e4847e699a0d71
Mashaqq <brawllola28@gmail.com>
2025-03-02 15:28:10 +0800

    'пон'

--- bot.py
+++ bot.py
@@ -2,11 +2,14 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters.command import Command
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
 from dotenv import dotenv_values
 from users.users import read_user_config, write_user_config, update_user_config, user_exists
-from keyboards.keyboards import start_keyboard, settings_keyboard
-config = dotenv_values(".env")
+from keyboards.keyboards import start_keyboard, settings_keyboard, addresses_keyboard
+from states.states import SettingStates
+from users.address import Address
 
+config = dotenv_values(".env")
 bot = Bot(token=config["BOT_TOKEN"])
 dp = Dispatcher()
 
@@ -44,11 +47,13 @@ async def back_to_menu(callback: CallbackQuery):
                                      reply_markup=start_keyboard())
     
 @dp.callback_query(F.data == 'name_change')
-async def name_change(callback: CallbackQuery):
+async def name_change(callback: CallbackQuery, state: FSMContext):
     await callback.message.edit_text(text='Введите новое имя:')
 
-@dp.message(F.text)
-async def new_name(message: Message):
+    await state.set_state(SettingStates.choose_name)
+
+@dp.message(F.text, SettingStates)
+async def new_name(message: Message, state: FSMContext):
     new_name = message.text
     user_id = message.from_user.id
     keys_to_update = {
@@ -56,6 +61,54 @@ async def new_name(message: Message):
     }
     update_user_config(user_id=user_id, keys_to_update=keys_to_update)
     await message.answer(text=f'{new_name}, Вашe имя успешно изменено!')
+    await state.set_state(None)
+
+@dp.callback_query(F.data=="addresses_settings")
+async def addresses_settings(callback: CallbackQuery, state: FSMContext):
+    user_id = callback.message.chat.id
+    config = read_user_config(user_id=user_id)
+
+    if len(config['addresses']) == 0:
+        await callback.message.edit_text('У вас не указан ни один адрес доставки.\nДобавьте адреса ниже:' 
+                                         reply_markup=addresses_keyboard())
+
+    await callback.message.edit_text('',)
+
+@dp.callback_query(F.data=='add_address')
+async def add_addresses(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text('Введите название нового адреса:')
+    await state.set_state(SettingStates.add_address_label)
+
+@dp.message(F.text, SettingStates)
+async def add_address_label(message: Message, state: FSMContext):
+    address_label = message.text
+
+    await message.answer(f'Укажите адрес для {address_label}')
+    await state.set_state(SettingStates.add_address_text)
+    await state.set_data('label': address_label)
+
+@dp.message(F.text, SettingStates.add_address_text)
+async def add_address_text(message: Message, state: FSMContext):
+    address = message.text
+
+    data = await state.get_data()
+    address_label = data('label')
+
+    user_id = message.from_user.id
+    config = read_user_config(user_id=user_id)
+
+    new_address = Address.fron_dict(
+        {
+            "label": address_label
+            "address": address
+        }
+    )
+
+    config['addresses'].append(new_address.to_dict())
+    keys_to_update = {
+        'addresses': config['addresses']
+    }
+    update_user_config(user_id=user_id, keys_to_update=keys_to_update)
 
 async def main():
     print('Я запущен!')
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -11,8 +11,19 @@ def settings_keyboard():
 
     builder.add(
         InlineKeyboardButton(text='Поменять имя', callback_data='name_change'),
-        InlineKeyboardButton(text='Изменить адрес', callback_data='addresses'),
+        InlineKeyboardButton(text='Добавить адрес', callback_data='address'),
         InlineKeyboardButton(text='Вернуться в меню', callback_data='back_to_menu')
     )
     builder.adjust(2, 1)
+    return builder.as_markup()
+
+def addresses_keyboard():
+    builder = InlineKeyboardBuilder()
+
+    builder.add(
+        InlineKeyboardButton(text='Добавить адрес', callback_data='add_address'),
+        InlineKeyboardButton(text='Вернуться в меню', callback_data='back_to_menu')
+    )
+
+    builder.adjust(1)
     return builder.as_markup()
